c1cd7ec9be1c5abb704f768d27348de792941ccf
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2020-04-04 16:20:53 -0700

    Functional interface
    (Not connected with scheduler, but the buttons work)

--- interfaceproject.py
+++ interfaceproject.py
@@ -12,15 +12,71 @@ def clear():
     for widget in win.winfo_children():
         widget.destroy()
 def add():
+    AddButton.pack_forget()
+    PracticeButton.pack_forget()
+    def notes():
+        def submit():
+            submit_button.destroy()
+            task_notes.destroy()
+            note_label.destroy() 
+            AddButton.pack(side=tk.LEFT)
+            PracticeButton.pack(side=tk.LEFT)
+        note_label = tk.Label(text='edit note or keep the same')
+        note_label.pack()
+
+        task_notes = tk.Text()
+        task_notes.pack()
+        task_notes.insert(tk.END,'old notes')
+        note = task_notes.get('1.0', tk.END)
+        submit_button = tk.Button(menu, text = 'submit notes', command=submit)
+        submit_button.pack()
+    def enter():
+        title = task_title.get()
+        print(repr(title))
+
+        text = task_text.get('1.0', tk.END)
+        print(repr(text))
+
+        task_title.delete(0,tk.END)
+        task_title.destroy()
+        task_text.destroy()
+        EnterButton.destroy()
+        notes()
+
+    #add_frame = tk.LabelFrame(master=win, text='add frame')
+    #add_frame.pack()
     task_title = tk.Entry(text='title', width=50)
     task_title.pack()
     task_text = tk.Text()
     task_text.pack()
-    task_title = task_title.get()
-    print(task_title)
-    tk.mainloop()
+    EnterButton = tk.Button(menu, text="Enter", command=enter)
+    EnterButton.pack()
+    #tk.mainloop()
 
 def practice():
+    AddButton.pack_forget()
+    PracticeButton.pack_forget()
+    def complete():
+        print('task completed!')
+        task.destroy()
+        complete_button.pack_forget()
+        cancel_button.pack_forget()
+        AddButton.pack(side=tk.LEFT)
+        PracticeButton.pack(side=tk.LEFT)
+    def cancel():
+        print('task cancelled :[')
+        task.destroy()
+        complete_button.pack_forget()
+        cancel_button.pack_forget()
+        AddButton.pack(side=tk.LEFT)
+        PracticeButton.pack(side=tk.LEFT)
+
+    complete_button = tk.Button(menu, text='Complete', command=complete)
+    cancel_button = tk.Button(menu, text='Cancel', command=cancel)
+    complete_button.pack(side=tk.LEFT)
+    cancel_button.pack(side=tk.LEFT)
+        
+        
     task = tk.Label(text=f'{random.random()}')
     task.pack()
 
@@ -36,6 +92,9 @@ AddButton.pack(side=tk.LEFT)
 PracticeButton = tk.Button(menu, text="Practice", command=practice)
 PracticeButton.pack(side=tk.LEFT)
 menu.pack()
+
+add_frame = tk.LabelFrame(master=win, text='add frame')
+add_frame.pack()
 
 ExitButton = tk.Button(win,text='Exit',command=exit)
 ExitButton.pack(side=tk.BOTTOM)
