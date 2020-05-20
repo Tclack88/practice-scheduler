05612d56bf82fab16a386bc19e46ffac76bcd606
Trevor Clack <tclack@aol.com>
2020-05-19 20:55:19 -0700

    incorporated drag and drop feature

--- pyqt_GUI/edit_gui.py
+++ pyqt_GUI/edit_gui.py
@@ -6,8 +6,64 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+import sys, os
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+
+class ImageLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+
+        self.setAlignment(Qt.AlignCenter)
+        self.setText('\n\n Drop Image Here \n\n')
+        self.setStyleSheet('''
+            QLabel{
+                border: 4px dashed #aaa
+            }
+        ''')
+
+    def setPixmap(self, image):
+        super().setPixmap(image)
+
+
+class DragNDropBox(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(400, 400)
+        self.setAcceptDrops(True)
+        mainLayout = QVBoxLayout()
+        self.photoViewer = ImageLabel()
+        mainLayout.addWidget(self.photoViewer)
+        self.setLayout(mainLayout)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasImage:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasImage:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasImage:
+            event.setDropAction(Qt.CopyAction)
+            file_path = event.mimeData().urls()[0].toLocalFile()
+            self.set_image(file_path)
+            event.accept()
+        else:
+            event.ignore()
+
+    def set_image(self, file_path):
+        self.photoViewer.setPixmap(QPixmap(file_path))
+       
 
 
 class Ui_MainWindow(object):
@@ -49,13 +105,17 @@ class Ui_MainWindow(object):
         self.notesBox = QtWidgets.QTextEdit(self.frame)
         self.notesBox.setGeometry(QtCore.QRect(50, 90, 501, 131))
         self.notesBox.setObjectName("notesBox")
-        self.imageWidget = QtWidgets.QWidget(self.frame)
+
+        #self.imageWidget = QtWidgets.QWidget(self.frame)
+        self.imageWidget = DragNDropBox() # change class as defined above
         self.imageWidget.setGeometry(QtCore.QRect(220, 230, 150, 150))
         self.imageWidget.setAcceptDrops(True)
         self.imageWidget.setAccessibleName("")
         self.imageWidget.setAutoFillBackground(False)
         self.imageWidget.setStyleSheet("border: 4px dashed #aaa;")
         self.imageWidget.setObjectName("imageWidget")
+        self.imageWidget.setParent(self.frame)
+
         self.addImageButton = QtWidgets.QPushButton(self.frame)
         self.addImageButton.setGeometry(QtCore.QRect(50, 220, 71, 25))
         self.addImageButton.setAutoDefault(False)
